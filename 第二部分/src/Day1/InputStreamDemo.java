fd1a8682ebd4bef8dd9fd1e3a98be08e11f07e60
IYYKI <2664047133@qq.com>
2019-01-04 23:01:50 +0800

    OutputStreamDemo

--- 第二部分/src/Day1/InputStreamDemo.java
+++ 第二部分/src/Day1/InputStreamDemo.java
@@ -8,6 +8,26 @@ public class InputStreamDemo {
     public static void main (String[]args){
 
     }
+    private static void readByBufferDemo()throws IOException{
+        InputStream in = new FileInputStream("demo2.txt");
+        byte [] buf = new byte[10];
+        //count 是读取的数据个数：1～10 > 0,如果-1到文件尾
+        int count;
+        while ((count = in.read(buf))!=-1){
+            System.out.println(toHexString(buf));
+        }
+        in.close();
+    }
+    private static String toHexString(byte[]ary){
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0;i<ary.length;i++){
+            byte b = ary[i];
+            int a = b & 0xff;
+          String hex = Integer.toHexString(b);
+          buf.append(hex).append(" ");
+        }
+        return buf.toString();
+    }
     public static void readDemo(String filename)throws IOException{
         InputStream in = new FileInputStream("demo.txt");
         //读取文件，的一个byte，无符号填充到 int 的低八位
