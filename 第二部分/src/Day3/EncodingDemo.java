1106f362173113ae424cfc29a1318a099c5d82e1
IYYKI <2664047133@qq.com>
2019-01-06 21:44:39 +0800

    EncodingDemo

---
+++ 第二部分/src/Day3/EncodingDemo.java
@@ -0,0 +1,31 @@
+package Day3;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class EncodingDemo {
+    public static void main (String [] args)throws IOException{
+        printFile("demo_gbk.txt");
+        printFile("demo_utf16be.txt");
+    }
+    /**
+     * 按照16进制的方式输出文件内容
+     * @param filename 文件名
+     * **/
+    public static void printFile(String filename)throws IOException{
+        File file = new File(filename);
+        RandomAccessFile raf = new RandomAccessFile(file,"r");
+        byte [] buf = new byte[(int)file.length()];
+        int size = raf.read(buf);
+        printHex(buf);//将byte 数组安装 hex 进制的形式打印
+        System.out.println("file size:"+size);
+    }
+    private static void printHex(byte[] buf) {
+        for (byte b : buf) {
+            String hex = Integer.toHexString(b & 0xff);
+            System.out.print(hex + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ 第二部分/src/Day3/IOUtils.java
@@ -0,0 +1,20 @@
+package Day3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
+
+public class IOUtils {
+    public static byte [] read (String filename)throws IOException {
+        return null;
+    }
+    public static String toHexString(byte [] b){
+        return null;
+    }
+    public static byte [] read(File  file)throws IOException{
+        return null;
+    }
+    public static byte [] read(InputStream in)throws IOException{
+        return null;
+    }
+}
